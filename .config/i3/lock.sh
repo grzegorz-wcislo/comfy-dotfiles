28501cdfbe9d4cada9062743ab5f2b59977cced0
Grzegorz Wcisło <grzegorz.oskar.wcislo@gmail.com>
2018-01-11 18:05:03 +0100

    Move sleep timer to systemd hook

--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -4,4 +4,3 @@ magick mogrify -scale 6.25% -paint 1 -scale 1600% /home/grzegorz/.cache/lock_img
 #magick mogrify -scale 12.5% -paint 3 -scale 800% /home/grzegorz/.cache/lock_img.png 
 #magick mogrify -paint 5 /home/grzegorz/.cache/lock_img.png 
 i3lock -e -i /home/grzegorz/.cache/lock_img.png
-sleep 0.2

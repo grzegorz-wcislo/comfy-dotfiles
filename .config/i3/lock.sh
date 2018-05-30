5147936bf61a903424e908cfb0414cfd15e97098
Grzegorz Wcisło <grzegorz.oskar.wcislo@gmail.com>
2018-05-30 22:01:33 +0200

    switch back to i3

--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 scrot -d 0 /home/grzegorz/.cache/lock_img.png
 magick mogrify -scale 6.25% -paint 1 -scale 1600% /home/grzegorz/.cache/lock_img.png 
-#magick mogrify -scale 12.5% -paint 3 -scale 800% /home/grzegorz/.cache/lock_img.png 
-#magick mogrify -paint 5 /home/grzegorz/.cache/lock_img.png 
 i3lock -e -i /home/grzegorz/.cache/lock_img.png
